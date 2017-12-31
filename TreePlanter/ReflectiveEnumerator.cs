064ea038e93ac66635f0b0a681cdad3fd1a5e002
thompcd <corey.dwayne.thompson@gmail.com>
2017-12-31 04:45:13 -0600

    Reflective enumerator is currently hardcoded purely for type "Tree" and needs to have its generics updated.

---
+++ TreePlanter/ReflectiveEnumerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace TreePlanter
+{
+    public static class ReflectiveEnumerator
+    {
+        public static List<Tree> GetEnumerableOfTypeTree()
+        {
+            List<Tree> objects = new List<Tree>();
+            foreach (Type type in
+                Assembly.GetAssembly(typeof(Tree)).GetTypes()
+                    .Where(myType => myType.IsClass && !myType.IsAbstract && myType.IsSubclassOf(typeof(Tree))))
+            {
+                objects.Add((Tree)Activator.CreateInstance(type));
+            }
+            //objects.Sort();
+            return objects;
+        }
+    }
+}
