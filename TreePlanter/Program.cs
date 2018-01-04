37fb434c55d0918c776b7ee0af994e1390568270
thompcd <corey.dwayne.thompson@gmail.com>
2018-01-03 21:38:36 -0600

    testing with reflection to create classes rather than manually instantiating.

--- TreePlanter/Program.cs
+++ TreePlanter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,23 @@ namespace TreePlanter
         {
             List<Tree> trees = new List<Tree>();
 
+            //trees = ReflectiveEnumerator.GetEnumerableOfTypeTree();
+            //List<Tree> objects = new List<Tree>();
+            //foreach (Type type in
+            //    Assembly.GetAssembly(typeof(Tree)).GetTypes()
+            //        .Where(myType => myType.IsClass && !myType.IsAbstract && myType.IsSubclassOf(typeof(Tree))))
+            //{
+            //    objects.Add((Tree)Activator.CreateInstance(type));
+            //}
+            //Console.Write("Via Reflection:");
+            //foreach (var tree in trees)
+            //{
+            //    string val = tree.ToString();
+            //    Type type = Type.GetType(val);
+            //    object instance = Activator.CreateInstance(type);
+            //
+            //    Console.WriteLine(tree.ToString());
+            //}
             Tree.SaucerMagnolia sm = new Tree.SaucerMagnolia();
             Tree.LittleGem lg = new Tree.LittleGem();
             Tree.OctoberGlory og = new Tree.OctoberGlory();
